367542f0ecd89eb6da81faaffc2e8453fd03545e
Dmitrii_Belov1 <Dmitrii_Belov1@epam.com>
2023-01-23 10:06:27 +0600

    Initial commit

---
+++ models/integration_pd.py
@@ -0,0 +1,63 @@
+import json
+from enum import Enum
+from json import JSONDecodeError
+from typing import Union
+
+from google.cloud import compute_v1
+from google.oauth2.service_account import Credentials
+from pydantic import BaseModel
+from pylon.core.tools import log
+
+from tools import session_project
+from ...integrations.models.pd.integration import SecretField
+
+
+class IntegrationModel(BaseModel):
+    service_account_info: Union[SecretField, str]
+    project: str
+    zone: str
+
+    def check_connection(self):
+        try:
+            service_info = json.loads(
+                self.service_account_info.unsecret(session_project.get()))
+            credentials = Credentials.from_service_account_info(service_info)
+            instance_client = compute_v1.InstancesClient(credentials=credentials)
+            instance_client.list(
+                project=self.project,
+                zone=self.zone,
+            )
+        except JSONDecodeError:
+            return "Failed to decode service account info"
+        except Exception as exc:
+            log.error(exc)
+            return str(exc)
+        return True
+
+    def get_zones(self):
+        service_info = json.loads(
+            self.service_account_info.unsecret(session_project.get()))
+        credentials = Credentials.from_service_account_info(service_info)
+        zones_client = compute_v1.ZonesClient(credentials=credentials)
+        zones = zones_client.list(project=self.project)
+        return [zone.name for zone in zones]
+
+
+class InstanceType(str, Enum):
+    on_demand = "on-demand"
+    spot = "spot"
+
+
+class PerformanceBackendTestModel(IntegrationModel):
+    id: int
+    cpu_cores_limit: int
+    memory_limit: int
+    concurrency: int
+    instance_type: InstanceType
+
+    class Config:
+        use_enum_values = True
+
+
+class PerformanceUiTestModel(PerformanceBackendTestModel):
+    ...
---
+++ static/js/integrations.js
@@ -0,0 +1,237 @@
+const GCPIntegration = {
+    delimiters: ['[[', ']]'],
+    props: ['instance_name', 'display_name', 'logo_src', 'section_name'],
+    emits: ['update'],
+    template: `
+<div
+        :id="modal_id"
+        class="modal modal-small fixed-left fade shadow-sm" tabindex="-1" role="dialog"
+        @dragover.prevent="modal_style = {'height': '300px', 'border': '2px dashed var(--basic)'}"
+        @drop.prevent="modal_style = {'height': '100px', 'border': ''}"
+>
+    <ModalDialog
+            v-model:description="description"
+            v-model:is_default="is_default"
+            @update="update"
+            @create="create"
+            :display_name="display_name"
+            :id="id"
+            :is_fetching="is_fetching"
+            :is_default="is_default"
+    >
+        <template #body>
+            <div class="form-group">
+                <h9>Project</h9>
+                <input type="text" 
+                       v-model="project" 
+                       class="form-control form-control-alternative"
+                       placeholder="Project identifier"
+                       :class="{ 'is-invalid': error.project }">
+                <div class="invalid-feedback">[[ error.project ]]</div>
+                <div>
+                <h9>Service account</h9>
+                 <SecretFieldInput
+                        ref="secretField"
+                        v-model="service_account_info"
+                        placeholder="Service account info"
+                 />
+                 <label class="mb-1">
+                    <span class="btn btn-secondary btn-sm mr-1 d-inline-block">Upload json</span>
+                    <input type="file" accept="application/json" 
+                    class="form-control form-control-alternative"
+                           style="display: none"
+                           @change="handleInputFile"
+                           :class="{ 'is-invalid': error.template }"
+                    >
+                </label>
+                <div class="invalid-feedback">[[ error.service_account_info ]]</div>
+                </div>
+                <h9>Zone</h9>
+                <input type="text" class="form-control form-control-alternative"
+                       v-model="zone"
+                       placeholder="GCP zone"
+                       :class="{ 'is-invalid': error.zone }">
+                <div class="invalid-feedback">[[ error.zone ]]</div>
+            </div>
+        </template>
+        <template #footer>
+            <test-connection-button
+                    :apiPath="api_base + 'check_settings/' + pluginName"
+                    :error="error.check_connection"
+                    :body_data="body_data"
+                    v-model:is_fetching="is_fetching"
+                    @handleError="handleError"
+            >
+            </test-connection-button>
+        </template>
+
+    </ModalDialog>
+</div>
+    `,
+    data() {
+        return this.initialState()
+    },
+    mounted() {
+        this.modal.on('hidden.bs.modal', e => {
+            this.clear()
+        })
+    },
+    computed: {
+        apiPath() {
+            return this.api_base + 'integration/'
+        },
+        project_id() {
+            return getSelectedProjectId()
+        },
+        body_data() {
+            const {
+                zone,
+                project,
+                service_account_info,
+                project_id,
+                description,
+                is_default,
+                status
+            } = this
+            return {
+                zone,
+                project,
+                service_account_info,
+                project_id,
+                description,
+                is_default,
+                status
+            }
+        },
+        modal() {
+            return $(this.$el)
+        },
+        modal_id() {
+            return `${this.instance_name}_integration`
+        }
+    },
+    methods: {
+        clear() {
+            Object.assign(this.$data, this.initialState())
+        },
+        load(stateData) {
+            Object.assign(this.$data, stateData)
+        },
+        handleEdit(data) {
+            const {description, is_default, id, settings} = data
+            this.load({...settings, description, is_default, id})
+            this.modal.modal('show')
+        },
+        handleDelete(id) {
+            this.load({id})
+            this.delete()
+        },
+        create() {
+            this.is_fetching = true
+            fetch(this.apiPath + this.pluginName, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(this.body_data)
+            }).then(response => {
+                this.is_fetching = false
+                if (response.ok) {
+                    this.modal.modal('hide')
+                    this.$emit('update', {...this.$data, section_name: this.section_name})
+                } else {
+                    this.handleError(response)
+                }
+            })
+        },
+        handleError(response) {
+            try {
+                response.json().then(
+                    errorData => {
+                        errorData.forEach(item => {
+                            this.error = {[item.loc[0]]: item.msg}
+                        })
+                    }
+                )
+            } catch (e) {
+                alertMain.add(e, 'danger-overlay')
+            }
+        },
+        update() {
+            this.is_fetching = true
+            fetch(this.apiPath + this.id, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(this.body_data)
+            }).then(response => {
+                this.is_fetching = false
+                if (response.ok) {
+                    this.modal.modal('hide')
+                    this.$emit('update', {...this.$data, section_name: this.section_name})
+                } else {
+                    this.handleError(response)
+                }
+            })
+        },
+        delete() {
+            this.is_fetching = true
+            fetch(this.apiPath + this.id, {
+                method: 'DELETE',
+            }).then(response => {
+                this.is_fetching = false
+
+                if (response.ok) {
+                    this.$emit('update', {...this.$data, section_name: this.section_name})
+                } else {
+                    this.handleError(response)
+                    alertMain.add(`
+                        Deletion error. 
+                        <button class="btn btn-primary" 
+                            onclick="vueVm.registered_components.${this.instance_name}.modal.modal('show')"
+                        >
+                            Open modal
+                        <button>
+                    `)
+                }
+            })
+        },
+        handleInputFile(event) {
+            const input = event.target
+            if (input.files && input.files[0]) {
+                this.handleFileUpload(input.files[0])
+            }
+        },
+        handleFileUpload(file) {
+            let reader = new FileReader()
+            reader.onload = (evt) => {
+                this.service_account_info = {
+                    value: evt.target.result,
+                    from_secrets: false
+                }
+                this.$refs['secretField'].from_secrets = false
+            }
+            reader.onerror = (e) => {
+                this.error.service_account_info = 'error reading file'
+                this.service_account_info = ''
+            }
+            delete this.error.service_account_info
+            reader.readAsText(file)
+        },
+
+        initialState: () => ({
+            modal_style: {'height': '100px', 'border': ''},
+            zone: "",
+            project: "",
+            test_key: 0,
+            service_account_info: "",
+            is_default: false,
+            is_fetching: false,
+            description: '',
+            error: {},
+            id: null,
+            pluginName: 'gcp_integration',
+            api_base: '/api/v1/integrations/',
+            status: integration_status.success,
+        })
+    }
+}
+
+register_component('GcpIntegration', GCPIntegration)
